388150f06f6ca9edff5593cddfe43c5ae6d0a415
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-27 17:27:31 +0100

    winner component created, displays winner message if game has been won, else it doesn't display anything

---
+++ src/components/Winner/Winner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Winner = ({ winner }) => (
+    winner !== 0 ?
+        <h2
+            className="alert alert-success"
+        >
+            {`Player ${winner} Wins!`}
+        </h2>
+        : null
+)
+
+export default Winner;
