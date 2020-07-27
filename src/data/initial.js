e3f7dd300ff650dca91d7def564de474d860a563
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-27 14:59:24 +0100

    app's initial state created

---
+++ src/data/initial.js
@@ -0,0 +1,17 @@
+const initial = {
+    gameId: "",
+    showSettings: true,
+    player1: 0,
+    player2: 0,
+    player1Name: "",
+    player2Name: "",
+    winningScore: 21,
+    serveInterval: 5,
+    server: 1,
+    winner: 0,
+    gameHistory: [],
+    historyLoaded: false, // for API request
+    language: "English"
+};
+
+export default initial;
