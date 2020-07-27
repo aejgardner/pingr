646a923e667dd4cbdbef3a3d1f656ac61cf2f97d
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-27 16:13:46 +0100

    header component created containing app title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Settings from "../Settings";
+import Header from "../Header";
 
 const App = ({
   showSettings
 }) => (
     <>
+
+      {/* header */}
+      <Header />
+
       <Settings />
     </>
   );
