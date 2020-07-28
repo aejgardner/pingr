0780a01fae03527f37f568675ddbb5e952f44b5d
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-28 11:58:16 +0100

    Updates

---
+++ precache-manifest.c277d30817540229942ea876153f78fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f420debf19139982ad8f8bca1b516c58",
+    "url": "/aejgardner/pingr/index.html"
+  },
+  {
+    "revision": "be7c9a53ca2d7437ad13",
+    "url": "/aejgardner/pingr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56cf42a97277022ebbab",
+    "url": "/aejgardner/pingr/static/js/2.3d10c696.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aejgardner/pingr/static/js/2.3d10c696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7c9a53ca2d7437ad13",
+    "url": "/aejgardner/pingr/static/js/main.ae387321.chunk.js"
+  },
+  {
+    "revision": "919a73a8e5692ff7ee11",
+    "url": "/aejgardner/pingr/static/js/runtime-main.4845c6f3.js"
+  }
+]);
